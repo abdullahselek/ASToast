d214e76fcee852549cd9dacd909ac1554c0c2f48
abdullahselek <abdullahselek@gmail.com>
2016-12-05 19:21:13 +0100

    - framework for carthage

---
+++ ASToastFramework/ASToastFramework.h
@@ -0,0 +1,19 @@
+//
+//  ASToastFramework.h
+//  ASToastFramework
+//
+//  Created by Abdullah Selek on 05/12/2016.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASToastFramework.
+FOUNDATION_EXPORT double ASToastFrameworkVersionNumber;
+
+//! Project version string for ASToastFramework.
+FOUNDATION_EXPORT const unsigned char ASToastFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASToastFramework/PublicHeader.h>
+
+
